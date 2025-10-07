8646e6546c95130551b661e6b1dd950ac6768046
wamunyima3 <wamunyimamukelabai3@gmail.com>
2025-10-07 16:45:00 +0200

    Create data models with JSON serialization support

---
+++ lib/src/models/api_response.dart
@@ -0,0 +1,230 @@
+import 'package:equatable/equatable.dart';
+import 'package:json_annotation/json_annotation.dart';
+
+part 'api_response.g.dart';
+
+/// Generic API response wrapper
+@JsonSerializable(genericArgumentFactories: true)
+class LencoApiResponse<T> extends Equatable {
+  final bool status;
+  final String message;
+  final T? data;
+  final String? errorCode;
+  final Map<String, dynamic>? errors;
+
+  const LencoApiResponse({
+    required this.status,
+    required this.message,
+    this.data,
+    this.errorCode,
+    this.errors,
+  });
+
+  factory LencoApiResponse.fromJson(
+    Map<String, dynamic> json,
+    T Function(Object? json) fromJsonT,
+  ) =>
+      _$LencoApiResponseFromJson(json, fromJsonT);
+
+  Map<String, dynamic> toJson(Object? Function(T value) toJsonT) =>
+      _$LencoApiResponseToJson(this, toJsonT);
+
+  @override
+  List<Object?> get props => [status, message, data, errorCode, errors];
+}
+
+/// Account model
+@JsonSerializable()
+class LencoAccount extends Equatable {
+  final String id;
+  final String name;
+  final BankAccount bankAccount;
+  final String type;
+  final String status;
+  final String availableBalance;
+  final String currentBalance;
+  final DateTime createdAt;
+  final String currency;
+
+  const LencoAccount({
+    required this.id,
+    required this.name,
+    required this.bankAccount,
+    required this.type,
+    required this.status,
+    required this.availableBalance,
+    required this.currentBalance,
+    required this.createdAt,
+    required this.currency,
+  });
+
+  factory LencoAccount.fromJson(Map<String, dynamic> json) =>
+      _$LencoAccountFromJson(json);
+
+  Map<String, dynamic> toJson() => _$LencoAccountToJson(this);
+
+  @override
+  List<Object?> get props => [
+        id,
+        name,
+        bankAccount,
+        type,
+        status,
+        availableBalance,
+        currentBalance,
+        createdAt,
+        currency,
+      ];
+}
+
+/// Bank account details
+@JsonSerializable()
+class BankAccount extends Equatable {
+  final String accountName;
+  final String accountNumber;
+  final Bank bank;
+
+  const BankAccount({
+    required this.accountName,
+    required this.accountNumber,
+    required this.bank,
+  });
+
+  factory BankAccount.fromJson(Map<String, dynamic> json) =>
+      _$BankAccountFromJson(json);
+
+  Map<String, dynamic> toJson() => _$BankAccountToJson(this);
+
+  @override
+  List<Object?> get props => [accountName, accountNumber, bank];
+}
+
+/// Bank model
+@JsonSerializable()
+class Bank extends Equatable {
+  final String code;
+  final String name;
+
+  const Bank({
+    required this.code,
+    required this.name,
+  });
+
+  factory Bank.fromJson(Map<String, dynamic> json) => _$BankFromJson(json);
+
+  Map<String, dynamic> toJson() => _$BankToJson(this);
+
+  @override
+  List<Object?> get props => [code, name];
+}
+
+/// Transaction model
+@JsonSerializable()
+class LencoTransaction extends Equatable {
+  final String id;
+  final String reference;
+  final String type;
+  final String amount;
+  final String currency;
+  final String status;
+  final String description;
+  final DateTime createdAt;
+  final DateTime? completedAt;
+  final String? recipientName;
+  final String? recipientAccount;
+
+  const LencoTransaction({
+    required this.id,
+    required this.reference,
+    required this.type,
+    required this.amount,
+    required this.currency,
+    required this.status,
+    required this.description,
+    required this.createdAt,
+    this.completedAt,
+    this.recipientName,
+    this.recipientAccount,
+  });
+
+  factory LencoTransaction.fromJson(Map<String, dynamic> json) =>
+      _$LencoTransactionFromJson(json);
+
+  Map<String, dynamic> toJson() => _$LencoTransactionToJson(this);
+
+  @override
+  List<Object?> get props => [
+        id,
+        reference,
+        type,
+        amount,
+        currency,
+        status,
+        description,
+        createdAt,
+        completedAt,
+        recipientName,
+        recipientAccount,
+      ];
+}
+
+/// Payment request model
+@JsonSerializable()
+class PaymentRequest extends Equatable {
+  final String accountId;
+  final String amount;
+  final String recipientAccountNumber;
+  final String recipientBankCode;
+  final String? narration;
+  final String? reference;
+
+  const PaymentRequest({
+    required this.accountId,
+    required this.amount,
+    required this.recipientAccountNumber,
+    required this.recipientBankCode,
+    this.narration,
+    this.reference,
+  });
+
+  factory PaymentRequest.fromJson(Map<String, dynamic> json) =>
+      _$PaymentRequestFromJson(json);
+
+  Map<String, dynamic> toJson() => _$PaymentRequestToJson(this);
+
+  @override
+  List<Object?> get props => [
+        accountId,
+        amount,
+        recipientAccountNumber,
+        recipientBankCode,
+        narration,
+        reference,
+      ];
+}
+
+/// Payment response model
+@JsonSerializable()
+class PaymentResponse extends Equatable {
+  final String id;
+  final String reference;
+  final String status;
+  final String amount;
+  final String? message;
+
+  const PaymentResponse({
+    required this.id,
+    required this.reference,
+    required this.status,
+    required this.amount,
+    this.message,
+  });
+
+  factory PaymentResponse.fromJson(Map<String, dynamic> json) =>
+      _$PaymentResponseFromJson(json);
+
+  Map<String, dynamic> toJson() => _$PaymentResponseToJson(this);
+
+  @override
+  List<Object?> get props => [id, reference, status, amount, message];
+}
